9f01a4e91e134c2644117ec38bc3de1311e7cb5c
Greco92 <russell@chanws.com>
2023-05-26 17:06:32 +0800

    Build summarizer app.

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-import pandas as pd
+# imports
 import streamlit as st
+from transformers import pipeline
 
 st.header('Welcome!')
+st.write('\n')
+st.write('\n')
 
-df = pd.DataFrame({'col_a':[1,2,3], 'col_b':['a', 'b', 'c']})
-st.dataframe(df)
+# cache model
+@st.cache_resource
+def init_summarizer_model():
+    summarizer_model = pipeline(task='summarization', model='facebook/bart-large-cnn')
+    return summarizer_model
+
+# define model
+with st.spinner('Loading'):
+    summarizer_model = init_summarizer_model()
+
+# text input
+st.write('Input text for summarization')
+sample_text = '''The MAS was founded in 1971 to oversee various monetary functions associated with banking and finance. 
+Before its establishment, monetary functions were performed by government departments and agencies. 
+The acronym for its name resembles mas, the word for 'gold' in Malay, Singapore's national language – although the acronym is pronounced with each of its initial alphabets.
+As Singapore progressed, an increasingly complex banking and monetary environment required more dynamic and coherent monetary administration. 
+Therefore, in 1970, the Parliament of Singapore passed the Monetary Authority of Singapore Act leading to the formation of MAS on 1 January 1971. 
+The act gives MAS the authority to regulate all elements of monetary policy, banking, and finance in Singapore.
+During the COVID-19 pandemic, MAS brought forward its twice yearly meeting from some time in April to 30 March. 
+The MAS decided to ease the Singapore dollar's appreciation rate to zero percent, as well as adjust the policy band downwards, the first such move since the Global Financial Crisis. 
+This makes it the first time the MAS had taken these two measures together.
+Unlike many central banks around the world, the MAS is not independent from the executive branch of the Singaporean government; chairmen of the MAS were from the same political party of the Government. 
+Previous chairmen were also either the incumbent or former Ministers of Finance, or were former Prime Ministers or Deputy Prime Ministers of Singapore.'''
+user_input = st.text_area(label='Paste your own text or use the sample provided.', value=sample_text)
+st.write('\n')
+st.write('\n')
+
+# summarize text
+summarized_text = summarizer_model(user_input)[0]['summary_text']
+
+# text output
+st.write('Here is your summarized text:')
+st.success(summarized_text)
