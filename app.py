3f77eb0ae0bca26d1f2d66f4d090f3d75d436d3e
Greco92 <russell@chanws.com>
2023-05-29 11:38:46 +0800

    Add inference via SageMaker Endpoint.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # imports
 import streamlit as st
 from transformers import pipeline
+import boto3
+import json
 
 st.header('Welcome!')
 st.write('\n')
@@ -9,7 +11,7 @@ st.write('\n')
 # cache model
 @st.cache_resource
 def init_summarizer_model():
-    summarizer_model = pipeline(task='summarization', model='facebook/bart-large-cnn')
+    summarizer_model = pipeline(task='summarization', model='google/pegasus-xsum')
     return summarizer_model
 
 # define model
@@ -17,10 +19,10 @@ with st.spinner('Loading'):
     summarizer_model = init_summarizer_model()
 
 # text input
-st.write('Input text for summarization')
+st.write('Input text for summarization:')
 sample_text = '''The MAS was founded in 1971 to oversee various monetary functions associated with banking and finance. 
 Before its establishment, monetary functions were performed by government departments and agencies. 
-The acronym for its name resembles mas, the word for 'gold' in Malay, Singapore's national language – although the acronym is pronounced with each of its initial alphabets.
+The acronym for its name resembles mas, the word for 'gold' in Malay, Singapore's national language - although the acronym is pronounced with each of its initial alphabets.
 As Singapore progressed, an increasingly complex banking and monetary environment required more dynamic and coherent monetary administration. 
 Therefore, in 1970, the Parliament of Singapore passed the Monetary Authority of Singapore Act leading to the formation of MAS on 1 January 1971. 
 The act gives MAS the authority to regulate all elements of monetary policy, banking, and finance in Singapore.
@@ -36,6 +38,24 @@ st.write('\n')
 # summarize text
 summarized_text = summarizer_model(user_input)[0]['summary_text']
 
-# text output
 st.write('Here is your summarized text:')
 st.success(summarized_text)
+st.write('\n')
+st.write('\n')
+
+
+st.write('Input SageMaker Model Endpoint to use for summarization:')
+# define sagemaker endpoint
+endpoint = st.text_input(label='SageMaker Model Endpoint')
+runtime = boto3.Session().client('sagemaker-runtime')
+payload = json.dumps({"inputs": sample_text}).encode('utf-8')
+
+# summarize text
+if endpoint != '':
+    try:
+        response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
+        endpoint_summarized_text = json.loads(response['Body'].read().decode())
+        
+        st.success(endpoint_summarized_text[0]['summary_text'])
+    except:
+        st.error('Error with Endpoint!')
