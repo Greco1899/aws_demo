a0e4c90a1b949170a4358629090aff7bd78e5ef9
Greco92 <russell@chanws.com>
2023-05-29 13:05:45 +0800

    Remove region

--- app.py
+++ app.py
@@ -54,8 +54,7 @@ if endpoint != '':
     runtime = boto3.Session(
         aws_access_key_id=os.environ.get('aws_access_key_id'), 
         aws_secret_access_key=os.environ.get('aws_secret_access_key'), 
-        region_name=os.environ.get('region_name')
-        ).client('sagemaker-runtime', region_name=os.environ.get('region_name'))
+        ).client('sagemaker-runtime')
     payload = json.dumps({"inputs": sample_text}).encode('utf-8')
     response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
     endpoint_summarized_text = json.loads(response['Body'].read().decode())
