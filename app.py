fceeccd2ce59b2ed3390af4e4f406a255dbf10a9
Greco <28339294+Greco1899@users.noreply.github.com>
2023-07-18 22:03:38 +0800

    Update app for summarization and extraction using LLMs

--- app.py
+++ app.py
@@ -4,24 +4,32 @@ from transformers import pipeline
 import boto3
 import json
 import os
-print('Imports Successful')
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+st.set_page_config(layout="wide")
 
-st.header('Welcome!')
+# Introduction
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown('# Mirage - SwiftScan')
+    st.markdown('## Instant insights from any report or text')
+    st.write('Start to summarize and question your document to get to what you really need!')
+with col2:
+    st.image('Mirage Logo.png', width=250)
 st.write('\n')
 st.write('\n')
 
-# cache model
-@st.cache_resource
-def init_summarizer_model():
-    summarizer_model = pipeline(task='summarization', model='sshleifer/distilbart-cnn-6-6')
-    return summarizer_model
+# # Cache model
+# @st.cache_resource
+# def init_summarizer_model():
+#     summarizer_model = pipeline(task='summarization', model='sshleifer/distilbart-cnn-6-6')
+#     return summarizer_model
 
-# define model
-with st.spinner('Loading'):
-    summarizer_model = init_summarizer_model()
+# # Define model
+# with st.spinner('Loading'):
+#     summarizer_model = init_summarizer_model()
 
-# text input
-st.write('Input text for summarization:')
+# Text input
+st.write('Input text for summarization and extraction:')
 sample_text = '''The MAS was founded in 1971 to oversee various monetary functions associated with banking and finance. 
 Before its establishment, monetary functions were performed by government departments and agencies. 
 The acronym for its name resembles mas, the word for 'gold' in Malay, Singapore's national language - although the acronym is pronounced with each of its initial alphabets.
@@ -33,44 +41,58 @@ The MAS decided to ease the Singapore dollar's appreciation rate to zero percent
 This makes it the first time the MAS had taken these two measures together.
 Unlike many central banks around the world, the MAS is not independent from the executive branch of the Singaporean government; chairmen of the MAS were from the same political party of the Government. 
 Previous chairmen were also either the incumbent or former Ministers of Finance, or were former Prime Ministers or Deputy Prime Ministers of Singapore.'''
-user_input = st.text_area(label='Paste your own text or use the sample provided.', value=sample_text)
+user_input = st.text_area(label='Paste your own text or use the sample provided.', value=sample_text, height=200)
 st.write('\n')
 st.write('\n')
 
-# summarize text
-summarized_text = summarizer_model(user_input)[0]['summary_text']
+# # Summarize text
+# summarized_text = summarizer_model(user_input)[0]['summary_text']
 
-st.write('Here is your summarized text:')
-st.success(summarized_text)
+# st.write('Here is your summarized text:')
+# st.success(summarized_text)
+# st.write('\n')
+# st.write('\n')
+
+st.write('What do you want to find out from the text?')
+user_question = st.text_input(label='Enter your own question or use the sample provided.', value='When was MAS founded?')
 st.write('\n')
 st.write('\n')
-print('Complete Default Summarization')
 
-st.write('Input SageMaker Model Endpoint to use for summarization:')
-# define sagemaker endpoint
-endpoint = st.text_input(label='SageMaker Model Endpoint')
+st.write('Select LLM to use for summarization and extraction:')
+llm_option = st.radio(label='Large Language Model', options=['Anthropic Claude 2.0', 'Falcon 40B'])
+st.write('\n')
+st.write('\n')
 
-# summarize text
-if endpoint != '':
-    payload = json.dumps({"inputs": sample_text}).encode('utf-8')
-    response = boto3.client('sagemaker-runtime').invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
-    endpoint_summarized_text = json.loads(response['Body'].read().decode())
-    st.success(endpoint_summarized_text[0]['summary_text'])
-else:
-    st.write('No Endpoint entered.')
+if llm_option == 'Anthropic Claude 2.0':
+    anthropic = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+    # anthropic = Anthropic(api_key='')
 
+    summarization_prompt='Summarize the following text as a short paragraph:'
+    completion = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=300,
+        prompt=f"{HUMAN_PROMPT} {summarization_prompt+sample_text} {AI_PROMPT}",
+    )
+    st.write('LLM Summary:')
+    st.success(completion.completion)
 
+    extraction_prompt='Given this CONTEXT, answer the following question. If you do not know the answer, just say that you do not know. Do not try to make up an answer.'
+    completion = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=300,
+        prompt=f"{HUMAN_PROMPT} {'CONTEXT:'+sample_text+extraction_prompt+user_question} {AI_PROMPT}",
+        )
+    st.write('LLM Response:')
+    st.success(completion.completion)
 
-st.write('Input SageMaker Model Endpoint to use for LLM prompting:')
-# define sagemaker endpoint
-llm_endpoint = st.text_input(label='SageMaker LLM Endpoint')
-user_input = st.text_input(label='User Prompt')
-                           
-# summarize text
-if llm_endpoint != '' and user_input != '':
+elif llm_option == 'Falcon 40B':
+    # llm_endpoint = ''
+    llm_endpoint = os.environ.get("FALCON_40B_ENDPOINT")
+
+    summarization_prompt='Summarize the following text as a short paragraph:'
     # define payload
     prompt = f"""You are an helpful Assistant, called Falcon.
-    User:{user_input}
+    User:{summarization_prompt+sample_text}
     Falcon:"""
 
     payload = {
@@ -85,11 +107,82 @@ if llm_endpoint != '' and user_input != '':
             "stop": ["\nUser:","<|endoftext|>","</s>"]
         }
     }
+
     # Inference
     response = boto3.client('sagemaker-runtime').invoke_endpoint(EndpointName=llm_endpoint, ContentType='application/json', Body=json.dumps(payload).encode('utf-8'))
-    assistant_reply = json.loads(response['Body'].read().decode())
-    st.success(assistant_reply[0]['generated_text'][len(prompt):])
-else:
-    st.write('No Endpoint and User Prompt entered.')
+    response = json.loads(response['Body'].read().decode())
+    st.write('LLM Summary:')
+    st.success(response[0]['generated_text'][len(prompt):])
 
-print('End of app')
+    extraction_prompt='Given this CONTEXT, answer the following question. If you do not know the answer, just say that you do not know. Do not try to make up an answer.'
+    # define payload
+    prompt = f"""You are an helpful Assistant, called Falcon.
+    User:{'CONTEXT'+sample_text+extraction_prompt+user_question}
+    Falcon:"""
+
+    payload = {
+        "inputs": prompt,
+        "parameters": {
+            "do_sample": True,
+            "top_k": 50,
+            "top_p": 0.2,
+            "temperature": 0.8,
+            "max_new_tokens": 1024,
+            "repetition_penalty": 1.03,
+            "stop": ["\nUser:","<|endoftext|>","</s>"]
+        }
+    }
+    
+    # Inference
+    response = boto3.client('sagemaker-runtime').invoke_endpoint(EndpointName=llm_endpoint, ContentType='application/json', Body=json.dumps(payload).encode('utf-8'))
+    response = json.loads(response['Body'].read().decode())
+    st.write('LLM Response:')
+    st.success(response[0]['generated_text'][len(prompt):])
+
+
+# st.write('Input LLM access key to use for summarization:')
+# # Define sagemaker endpoint
+# endpoint = st.text_input(label='SageMaker Model Endpoint')
+# # Define model type
+
+# # summarize text
+# if endpoint != '':
+#     payload = json.dumps({"inputs": sample_text}).encode('utf-8')
+#     response = boto3.client('sagemaker-runtime').invoke_endpoint(EndpointName=endpoint, ContentType='application/json', Body=payload)
+#     endpoint_summarized_text = json.loads(response['Body'].read().decode())
+#     st.success(endpoint_summarized_text[0]['summary_text'])
+# else:
+#     st.write('No Endpoint entered.')
+
+
+
+# st.write('Input SageMaker Model Endpoint to use for LLM prompting:')
+# # define sagemaker endpoint
+# llm_endpoint = st.text_input(label='SageMaker LLM Endpoint')
+# user_input = st.text_input(label='User Prompt')
+                           
+# # summarize text
+# if llm_endpoint != '' and user_input != '':
+#     # define payload
+#     prompt = f"""You are an helpful Assistant, called Falcon.
+#     User:{user_input}
+#     Falcon:"""
+
+#     payload = {
+#         "inputs": prompt,
+#         "parameters": {
+#             "do_sample": True,
+#             "top_k": 50,
+#             "top_p": 0.2,
+#             "temperature": 0.8,
+#             "max_new_tokens": 1024,
+#             "repetition_penalty": 1.03,
+#             "stop": ["\nUser:","<|endoftext|>","</s>"]
+#         }
+#     }
+#     # Inference
+#     response = boto3.client('sagemaker-runtime').invoke_endpoint(EndpointName=llm_endpoint, ContentType='application/json', Body=json.dumps(payload).encode('utf-8'))
+#     assistant_reply = json.loads(response['Body'].read().decode())
+#     st.success(assistant_reply[0]['generated_text'][len(prompt):])
+# else:
+#     st.write('No Endpoint and User Prompt entered.')
